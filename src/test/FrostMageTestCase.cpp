c38cb8795c9b4b0bca6f28c3aa1c770cc6e15490
ike3 <ike@email.org>
2016-11-29 20:36:21 +0300

    [PLAYERBOTS] remove speed condition in fleeing

--- src/test/FrostMageTestCase.cpp
+++ src/test/FrostMageTestCase.cpp
@@ -43,14 +43,12 @@ protected:
 
 		tickWithLowHealth(19);
 
-		context->GetValue<uint8>("speed", "current target")->Set(100);
         spellAvailable("frost nova");
         spellAvailable("frostbolt");
         tickInMeleeRange();
         tickInMeleeRange();
-        context->GetValue<uint8>("speed", "current target")->Set(80);
 
-		assertActions(">T:frostbolt>T:frost nova>S:flee>T:frostbolt>T:shoot>S:ice block>T:frost nova>T:frostbolt");
+		assertActions(">T:frostbolt>T:frost nova>S:flee>T:frostbolt>T:shoot>S:ice block>T:frost nova>S:flee");
 	}
 
     void dispel()
