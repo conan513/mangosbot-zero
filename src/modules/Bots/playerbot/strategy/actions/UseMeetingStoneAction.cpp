6275cb262adfab312450e046811cd20ff9eb0434
ike3 <ike@email.org>
2016-12-03 21:26:12 +0300

    [PLAYERBOTS] Summon bots near meeting stones

--- src/modules/Bots/playerbot/strategy/actions/UseMeetingStoneAction.cpp
+++ src/modules/Bots/playerbot/strategy/actions/UseMeetingStoneAction.cpp
@@ -3,6 +3,12 @@
 #include "UseMeetingStoneAction.h"
 #include "../../PlayerbotAIConfig.h"
 
+#include "GridNotifiers.h"
+#include "GridNotifiersImpl.h"
+#include "CellImpl.h"
+
+using namespace MaNGOS;
+
 bool UseMeetingStoneAction::Execute(Event event)
 {
     Player* master = GetMaster();
@@ -44,6 +50,24 @@ bool UseMeetingStoneAction::Execute(Event event)
     return Teleport();
 }
 
+class AnyGameObjectInObjectRangeCheck
+{
+public:
+    AnyGameObjectInObjectRangeCheck(WorldObject const* obj, float range) : i_obj(obj), i_range(range) {}
+    WorldObject const& GetFocusObject() const { return *i_obj; }
+    bool operator()(GameObject* u)
+    {
+        if (u && i_obj->IsWithinDistInMap(u, i_range) && u->isSpawned() && u->GetGOInfo())
+            return true;
+
+        return false;
+    }
+
+private:
+    WorldObject const* i_obj;
+    float i_range;
+};
+
 
 bool SummonAction::Execute(Event event)
 {
@@ -51,13 +75,25 @@ bool SummonAction::Execute(Event event)
     if (!master)
         return false;
 
-    if (master->GetSession()->GetSecurity() < SEC_GAMEMASTER)
+    if (master->GetSession()->GetSecurity() >= SEC_GAMEMASTER)
+        return Teleport();
+
+    list<GameObject*> targets;
+
+    AnyGameObjectInObjectRangeCheck u_check(master, sPlayerbotAIConfig.sightDistance);
+    GameObjectListSearcher<AnyGameObjectInObjectRangeCheck> searcher(targets, u_check);
+    Cell::VisitAllObjects((const WorldObject*)master, searcher, sPlayerbotAIConfig.sightDistance);
+
+    list<ObjectGuid> result;
+    for(list<GameObject*>::iterator tIter = targets.begin(); tIter != targets.end(); ++tIter)
     {
-        ai->TellMasterNoFacing("You cannot summon me");
-        return false;
+        GameObject* go = *tIter;
+        if (go && go->isSpawned() && go->GetGoType() == GAMEOBJECT_TYPE_MEETINGSTONE)
+            return Teleport();
     }
 
-    return Teleport();
+    ai->TellMasterNoFacing("There are no meeting stone nearby");
+    return false;
 }
 
 bool SummonAction::Teleport()
