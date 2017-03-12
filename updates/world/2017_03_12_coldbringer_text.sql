e660b1a334bd3e157abd5332cd9ce3644cdc10b8
lee1991 <littlelee_1991@live.co.uk>
2017-03-12 13:50:17 +0000

    Added scriptbinding for coldbringer

--- updates/world/2017_03_12_coldbringer_text.sql
+++ updates/world/2017_03_12_coldbringer_text.sql
@@ -4,9 +4,12 @@ DELETE FROM `script_texts` WHERE `entry` = -1129002;
 DELETE FROM `script_texts` WHERE `entry` = -1129001;
 DELETE FROM `script_texts` WHERE `entry` = -1129000;
 
+INSERT INTO `script_binding`(`type`, `ScriptName`, `bind`, `data`) VALUES (0, 'boss_coldbringer', 7358, 0);
+
 INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES 
 (-1129004, 'Too...easy!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5826, 1, 0, 0, 'amnennar SAY_KILL'),
 (-1129002, 'Come, spirits, attend your master!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5829, 1, 0, 0, 'amnennar SAY_SUMMON30'),
 (-1129003, 'I am the hand of the Lich King!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5827, 1, 0, 0, 'amnennar SAY_HP'),
 (-1129001, 'To me, my servants!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5828, 1, 0, 0, 'amnennar SAY_SUMMON60'),
 (-1129000, 'You\'ll never leave this place... alive.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5825, 1, 0, 0, 'amnennar SAY_AGGRO');
+
