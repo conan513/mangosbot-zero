32bfd6ba9b201f4f1860c07be900e2ca1a4321e8
ike3 <ike3@server.org>
2017-06-26 16:25:01 +0300

    [PLAYERBOTS] 'mail take' command to take all items from bot's mailbox (the bot must be near a mailbox)

--- src/modules/Bots/playerbot/strategy/actions/MailAction.cpp
+++ src/modules/Bots/playerbot/strategy/actions/MailAction.cpp
@@ -13,18 +13,18 @@ bool MailAction::Execute(Event event)
         return false;
 
     list<ObjectGuid> gos = *context->GetValue<list<ObjectGuid> >("nearest game objects");
-    bool mailboxFound = false;
+    ObjectGuid mailbox;
     for (list<ObjectGuid>::iterator i = gos.begin(); i != gos.end(); ++i)
     {
         GameObject* go = ai->GetGameObject(*i);
         if (go && go->GetGoType() == GAMEOBJECT_TYPE_MAILBOX)
         {
-            mailboxFound = true;
+            mailbox = go->GetObjectGuid();
             break;
         }
     }
 
-    if (!mailboxFound)
+    if (!mailbox)
     {
         ai->TellMaster("There is no mailbox nearby");
         return false;
@@ -45,7 +45,7 @@ bool MailAction::Execute(Event event)
             ostringstream out;
             out << "|cffffffff" << mail->subject;
             if (mail->money)
-                out << " " << ChatHelper::formatMoney(mail->money);
+                out << ", |cffffff00" << ChatHelper::formatMoney(mail->money);
             if (mail->has_items)
             {
                 for (MailItemInfoVec::iterator i = mail->items.begin(); i != mail->items.end(); ++i)
@@ -60,6 +60,53 @@ bool MailAction::Execute(Event event)
         }
         return true;
     }
+    else if (text == "take")
+    {
+        time_t cur_time = time(0);
+        for (PlayerMails::iterator itr = bot->GetMailBegin(); itr != bot->GetMailEnd(); ++itr)
+        {
+            if ((*itr)->state == MAIL_STATE_DELETED || cur_time < (*itr)->deliver_time)
+                continue;
 
-    return false;
+            Mail *mail = *itr;
+            if (mail->money)
+            {
+                ostringstream out;
+                out << mail->subject << ", |cffffff00" << ChatHelper::formatMoney(mail->money);
+                ai->TellMaster(out.str());
+
+                WorldPacket packet;
+                packet << mailbox;
+                packet << mail->messageID;
+                bot->GetSession()->HandleMailTakeMoney(packet);
+                bot->RemoveMail(mail->messageID);
+                continue;
+            }
+
+            if (!mail->items.empty())
+            {
+                for (MailItemInfoVec::iterator i = mail->items.begin(); i != mail->items.end(); ++i)
+                {
+                    ItemPrototype const *proto = sObjectMgr.GetItemPrototype(i->item_template);
+                    if (proto)
+                    {
+                        ostringstream out;
+                        out << mail->subject << ", " << ChatHelper::formatItem(proto);
+                        ai->TellMaster(out.str());
+                    }
+                }
+
+                WorldPacket packet;
+                packet << mailbox;
+                packet << mail->messageID;
+                bot->GetSession()->HandleMailTakeItem(packet);
+                bot->RemoveMail(mail->messageID);
+                continue;
+            }
+        }
+    }
+    else
+        ai->TellMaster("whisper 'mail ?' to query mailbox, 'mail take' to take all items");
+
+    return true;
 }
