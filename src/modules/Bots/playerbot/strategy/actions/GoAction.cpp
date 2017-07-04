7343b8c3ad38339300fcd12b268d60353d4216fe
ike3 <ike3@server.org>
2017-07-04 10:15:42 +0300

    [PLAYERBOTS] go <position> command

--- src/modules/Bots/playerbot/strategy/actions/GoAction.cpp
+++ src/modules/Bots/playerbot/strategy/actions/GoAction.cpp
@@ -3,6 +3,7 @@
 #include "GoAction.h"
 #include "../../PlayerbotAIConfig.h"
 #include "../values/Formations.h"
+#include "../values/PositionValue.h"
 
 using namespace ai;
 
@@ -40,46 +41,64 @@ bool GoAction::Execute(Event event)
                     return false;
                 }
 
+                ostringstream out; out << "Moving to " << ChatHelper::formatGameobject(go);
+                ai->TellMasterNoFacing(out.str());
                 return MoveNear(bot->GetMapId(), go->GetPositionX(), go->GetPositionY(), go->GetPositionZ() + 0.5f, sPlayerbotAIConfig.followDistance);
             }
         }
         return false;
     }
 
-    if (param.find(",") == string::npos)
+    if (param.find(",") != string::npos)
     {
-        ai->TellMaster("Whisper 'go x,y' in map coords and I'll go there");
-        return false;
+        vector<string> coords = split(param, ',');
+        float x = atof(coords[0].c_str());
+        float y = atof(coords[1].c_str());
+        Zone2MapCoordinates(x, y, bot->GetZoneId());
+
+        Map* map = bot->GetMap();
+        float z = bot->GetPositionZ();
+        bot->UpdateGroundPositionZ(x, y, z);
+
+        if (bot->GetDistance2d(x, y) > sPlayerbotAIConfig.reactDistance)
+        {
+            ai->TellMaster("It is too far away");
+            return false;
+        }
+
+        const TerrainInfo* terrain = map->GetTerrain();
+        if (terrain->IsUnderWater(x, y, z) || terrain->IsInWater(x, y, z))
+        {
+            ai->TellMaster("It is under water");
+            return false;
+        }
+
+        float ground = map->GetHeight(x, y, z + 0.5f);
+        if (ground <= INVALID_HEIGHT)
+        {
+            ai->TellMaster("I can't go there");
+            return false;
+        }
+
+        ostringstream out; out << "Moving to " << x << "," << y;
+        ai->TellMasterNoFacing(out.str());
+        return MoveNear(bot->GetMapId(), x, y, z + 0.5f, sPlayerbotAIConfig.followDistance);
     }
 
-    vector<string> coords = split(param, ',');
-    float x = atof(coords[0].c_str());
-    float y = atof(coords[1].c_str());
-    Zone2MapCoordinates(x, y, bot->GetZoneId());
-
-    Map* map = bot->GetMap();
-    float z = bot->GetPositionZ();
-    bot->UpdateGroundPositionZ(x, y, z);
-
-    if (bot->GetDistance2d(x, y) > sPlayerbotAIConfig.reactDistance)
+    ai::Position& pos = context->GetValue<ai::Position&>("position", param)->Get();
+    if (pos.isSet())
     {
-        ai->TellMaster("It is too far away");
-        return false;
+        if (bot->GetDistance2d(pos.x, pos.y) > sPlayerbotAIConfig.reactDistance)
+        {
+            ai->TellMaster("It is too far away");
+            return false;
+        }
+
+        ostringstream out; out << "Moving to position " << param;
+        ai->TellMasterNoFacing(out.str());
+        return MoveNear(bot->GetMapId(), pos.x, pos.y, pos.z + 0.5f, sPlayerbotAIConfig.followDistance);
     }
 
-    const TerrainInfo* terrain = map->GetTerrain();
-    if (terrain->IsUnderWater(x, y, z) || terrain->IsInWater(x, y, z))
-    {
-        ai->TellMaster("It is under water");
-        return false;
-    }
-
-    float ground = map->GetHeight(x, y, z + 0.5f);
-    if (ground <= INVALID_HEIGHT)
-    {
-        ai->TellMaster("I can't go there");
-        return false;
-    }
-
-    return MoveNear(bot->GetMapId(), x, y, z + 0.5f, sPlayerbotAIConfig.followDistance);
+    ai->TellMaster("Whisper 'go x,y', 'go [game object]' or 'go position' and I will go there");
+    return false;
 }
