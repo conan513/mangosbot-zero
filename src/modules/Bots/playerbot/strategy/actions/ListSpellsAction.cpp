710dc27d38fb7fc8d24d545852a4b7841c8bdafd
ike3 <ike@email.org>
2017-07-09 11:03:39 +0300

    [PLAYERBOTS] spells <skill> command

--- src/modules/Bots/playerbot/strategy/actions/ListSpellsAction.cpp
+++ src/modules/Bots/playerbot/strategy/actions/ListSpellsAction.cpp
@@ -5,6 +5,56 @@
 
 using namespace ai;
 
+int SortSpells(pair<uint32, string>& s1, pair<uint32, string>& s2)
+{
+    int p1 = IsPositiveSpell(s1.first) ? 1 : 0;
+    int p2 = IsPositiveSpell(s2.first) ? 1 : 0;
+    if (p1 == p2)
+    {
+        const SpellEntry* const si1 = sSpellStore.LookupEntry(s1.first);
+        const SpellEntry* const si2 = sSpellStore.LookupEntry(s2.first);
+        p1 = si1->School;
+        p2 = si2->School;
+
+        if (p1 == p2)
+        {
+            uint32 skill1 = 0, skill2 = 0;
+            uint32 skillValue1 = 0, skillValue2 = 0;
+            for (uint32 j = 0; j < sSkillLineAbilityStore.GetNumRows(); ++j)
+            {
+                SkillLineAbilityEntry const* skillLine = sSkillLineAbilityStore.LookupEntry(j);
+                if (skillLine && skillLine->spellId == s1.first)
+                {
+                    skill1 = skillLine->skillId;
+                    skillValue1 = skillLine->min_value;
+                }
+                if (skillLine && skillLine->spellId == s2.first)
+                {
+                    skill2 = skillLine->skillId;
+                    skillValue2 = skillLine->min_value;
+                }
+                if (skill1 && skill2) break;
+            }
+
+            p1 = skill1;
+            p2 = skill2;
+            if (p1 == p2)
+            {
+                p1 = skillValue1;
+                p2 = skillValue2;
+
+                if (p1 == p2)
+                {
+                    return strcmp(si1->SpellName[0], si1->SpellName[1]);
+                }
+            }
+
+        }
+    }
+
+    return p1 = p2;
+}
+
 bool ListSpellsAction::Execute(Event event)
 {
     Player* master = GetMaster();
@@ -17,10 +67,23 @@ bool ListSpellsAction::Execute(Event event)
     std::ostringstream negOut;
 
     string filter = event.getParam();
+    uint32 skill = chat->parseSkill(filter);
+    if (skill != SKILL_NONE) filter = "";
+
+    list<uint32> skillSpells;
+    for (uint32 j = 0; j < sSkillLineAbilityStore.GetNumRows(); ++j)
+    {
+        SkillLineAbilityEntry const* skillLine = sSkillLineAbilityStore.LookupEntry(j);
+        if (!skillLine || skillLine->skillId != skill)
+            continue;
+
+        skillSpells.push_back(skillLine->spellId);
+    }
 
     const std::string ignoreList = ",Opening,Closing,Stuck,Remove Insignia,Opening - No Text,Grovel,Duel,Honorless Target,";
     std::string alreadySeenList = ",";
 
+    list<pair<uint32, string> > spells;
     for (PlayerSpellMap::iterator itr = bot->GetSpellMap().begin(); itr != bot->GetSpellMap().end(); ++itr) {
         const uint32 spellId = itr->first;
 
@@ -31,34 +94,53 @@ bool ListSpellsAction::Execute(Event event)
         if (!pSpellInfo)
             continue;
 
-        //|| name.find("Teleport") != -1
+        if (skill != SKILL_NONE && find(skillSpells.begin(), skillSpells.end(), spellId) == skillSpells.end())
+            continue;
 
-        std::string comp = ",";
-        comp.append(pSpellInfo->SpellName[loc]);
-        comp.append(",");
+        if (skill == SKILL_NONE && find(skillSpells.begin(), skillSpells.end(), spellId) != skillSpells.end())
+            continue;
 
+        string comp = pSpellInfo->SpellName[0];
         if (!(ignoreList.find(comp) == std::string::npos && alreadySeenList.find(comp) == std::string::npos))
             continue;
 
         if (!filter.empty() && !strstri(pSpellInfo->SpellName[loc], filter.c_str()))
             continue;
 
+        ostringstream out;
+        out << chat->formatSpell(pSpellInfo);
+
+        bool first = true;
+        for (uint32 x = 0; x < MAX_SPELL_REAGENTS; ++x)
+        {
+            if (pSpellInfo->Reagent[x] <= 0)
+                { continue; }
+
+            uint32 itemid = pSpellInfo->Reagent[x];
+            uint32 itemcount = pSpellInfo->ReagentCount[x];
+            if (itemid)
+            {
+                ItemPrototype const* proto = sObjectMgr.GetItemPrototype(itemid);
+                if (proto)
+                {
+                    if (first) { out << ": "; first = false; } else out << ", ";
+                    out << chat->formatItem(proto, itemcount);
+                }
+            }
+        }
+
+        spells.push_back(pair<uint32, string>(spellId, out.str()));
         alreadySeenList += pSpellInfo->SpellName[loc];
         alreadySeenList += ",";
-
-        if (IsPositiveSpell(spellId))
-            posOut << " |cffffffff|Hspell:" << spellId << "|h["
-            << pSpellInfo->SpellName[loc] << "]|h|r";
-        else
-            negOut << " |cffffffff|Hspell:" << spellId << "|h["
-            << pSpellInfo->SpellName[loc] << "]|h|r";
     }
 
-    ai->TellMaster("here's my non-attack spells:");
-    ai->TellMaster(posOut);
+    ai->TellMaster("=== Spells ===");
+    spells.sort(SortSpells);
 
-    ai->TellMaster("here's my attack spells:");
-    ai->TellMaster(negOut);
+    for (list<pair<uint32, string> >::iterator i = spells.begin(); i != spells.end(); ++i)
+    {
+        ai->TellMaster(i->second);
+    }
 
     return true;
 }
