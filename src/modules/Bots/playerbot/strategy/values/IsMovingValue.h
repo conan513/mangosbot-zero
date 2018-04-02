484ba681eb8d05d3d3933ce4e910f261eda797fd
ike3 <ike3@email.org>
2018-04-02 13:28:34 +0300

    [PLAYERBOTS] Change "target is moving" to fire only if target is fleeing or chasing non-tank players

--- src/modules/Bots/playerbot/strategy/values/IsMovingValue.h
+++ src/modules/Bots/playerbot/strategy/values/IsMovingValue.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Value.h"
+#include "MotionGenerators/TargetedMovementGenerator.h"
 
 namespace ai
 {
@@ -11,16 +12,25 @@ namespace ai
         virtual bool Calculate()
         {
             Unit* target = AI_VALUE(Unit*, qualifier);
+            Unit* chaseTarget;
 
             if (!target)
                 return false;
 
             switch (target->GetMotionMaster()->GetCurrentMovementGeneratorType())
             {
-            case IDLE_MOTION_TYPE:
-                return false;
+            case FLEEING_MOTION_TYPE:
+                return true;
+            case CHASE_MOTION_TYPE:
+                if (target->GetTypeId() == TYPEID_PLAYER)
+                    { chaseTarget = static_cast<ChaseMovementGenerator<Player> const*>(target->GetMotionMaster()->GetCurrent())->GetTarget(); }
+                else
+                    { chaseTarget = static_cast<ChaseMovementGenerator<Creature> const*>(target->GetMotionMaster()->GetCurrent())->GetTarget(); }
+
+                Player* chaseTargetPlayer = sObjectMgr.GetPlayer(chaseTarget->GetObjectGuid());
+                return chaseTargetPlayer && !ai->IsTank(chaseTargetPlayer);
             }
-            return true;
+            return false;
         }
     };
 
